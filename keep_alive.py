21bb3302144a5b55a85f6766b591f4424f80325b
Kuba_Kowalczyk <kubakowalczyk2006@wp.pl>
2025-04-18 21:36:06 +0200

    gowno 3

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,12 @@
-# keep_alive.py
+import os
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot is alive!"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
+    return 'Bot działa!'
 
 def keep_alive():
-    t = Thread(target=run)
-    t.start()
+    port = int(os.environ.get('PORT', 8080))  # Jeśli nie ma ustawionego portu, użyj 8080
+    app.run(host='0.0.0.0', port=port)
